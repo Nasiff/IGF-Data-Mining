d336737d1f9600e9687101b0f4b57f0b18efb3ae
Md Nasiful Haque <nasif_fcb_haque@yahoo.com>
2018-11-11 10:51:11 -0500

    Added the remaining scrapers
    Scrapers for 2006, 2007 and 2010. Some scrapers worked with multiple years and so there is a few scrapers missing

---
+++ webscrape2006.py
@@ -0,0 +1,72 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
+
+
+# The directory where all the transcripts are to saved after mining
+FILE_DIR = '/Users/User/Desktop/Mine/Workspace/vs_workspace/IGF_Mining/Transcripts_2006/'
+
+# send a GET request, pose as a Mozilla Firefox agent
+url = "http://www.intgovforum.org/multilingual/content/first-igf-meeting-athens-greece"
+headers = {'User-Agent' : 'Mozilla/5.0'}
+print("Establishing connection to: " + url)
+print(headers)
+r = requests.get(url, headers=headers)
+html = r.text
+
+# Convert html into BS object
+soup = BeautifulSoup(html, 'html.parser')
+hrefs = soup.find_all('a')
+
+# get all the links from the main page into a list
+print("Getting all the links on the page...")
+link_list = []
+for link in hrefs: 
+    links = link.get('href')
+    link_list.append(links)
+print (links)
+
+# identify the links of interest by examining the link_list
+begin = "http://www.intgovforum.org/cms/IGF-OpeningSession-301006.txt"
+end = "http://www.intgovforum.org/cms/IGF-Closing%20Ceremony.txt"
+begin_index = link_list.index(begin)
+end_index = link_list.index(end)
+
+# compose the cleaned out list with only the relevant links
+print("Finding transcript only links...")
+revised_list = link_list[begin_index:end_index+1]
+
+# after creating a list of lists, take only the 4th element and append it to a finalized list
+print(revised_list)
+file_num = 0
+
+for element in revised_list:
+    file_num = file_num + 1
+    headers = {'User-Agent' : 'Mozilla/5.0'}
+    print("Establishing connection to: " + element)
+    print(headers)
+    r = requests.get(element, headers=headers)
+    transcript_text = r.text
+    # print("*******************************************************")
+    # print(transcript_text)
+    file_name = element.split("/")[-1]
+    file_name = unquote(file_name)
+
+    # try:
+    fob = open(FILE_DIR + str(file_name), 'w')
+    fob.write(transcript_text)
+    fob.close()
+    print('Extracted and wrote transcript ' + str(file_name) + ' to text file')
+    # except UnicodeEncodeError:
+    #     pass
+
+# file_num0 = 0
+# split_list = revised_list.split("/")
+
+# for item in split_list:
+#     file_num0 = file_num0 + 1
+#     if file_num0 != 4:
+#         old_file = FILE_DIR + 'file_' + str(file_num0) + '.txt'
+#         new_file = FILE_DIR + str(item[5]) + '.txt'
+#         os.rename(old_file, new_file)
---
+++ webscrape2010.py
@@ -0,0 +1,98 @@
+import requests
+import os
+import sys
+import re
+from bs4 import BeautifulSoup
+
+# The directory where all the transcripts are to saved after mining
+FILE_DIR = '/Users/User/Desktop/Mine/Workspace/vs_workspace/IGF_Mining/Transcripts_2010/'
+
+# send a GET request, pose as a Mozilla Firefox agent
+url = "http://www.intgovforum.org/cms/component/content/article/96-vilnius-2010-meeting-events/625-transcripts"
+headers = {'User-Agent' : 'Mozilla/5.0'}
+print("Establishing connection to: " + url)
+print(headers)
+r = requests.get(url, headers=headers)
+html = r.text
+
+# Convert html into BS object
+soup = BeautifulSoup(html, 'html.parser')
+hrefs = soup.find_all('a')
+
+# Get all the text from the links to rename the mined files appropriately later
+text_list = []
+for a in hrefs:
+    pattern = re.compile('\W')
+    cleanString = re.sub(pattern, ' ', str(a.string))
+    text_list.append(cleanString)
+
+startIndex = text_list.index("morning session")
+endIndex = text_list.index("Round table for National and Regional meetings")
+text_list = text_list[startIndex:endIndex + 1]
+
+# get all the links from the main page into a list
+print("Getting all the links on the page...")
+link_list = []
+for link in hrefs: 
+    links = link.get('href')
+    link_list.append(links)
+print (links)
+
+# identify the links of interest by examining the link_list
+begin = "/cms/component/content/article/102-transcripts2010/759-magm"
+end = "/cms/component/content/article/102-transcripts2010/730-round-table"
+begin_index = link_list.index(begin)
+end_index = link_list.index(end)
+
+# compose the cleaned out list with only the relevant links
+print("Finding transcript only links...")
+revised_list = link_list[begin_index:end_index+1]
+
+print (str(len(text_list)) + " texts and " + str(len(revised_list)) + " links")
+
+# concatenate the extension of the relevant links and the base url to be able to
+# access those links later on, print the result in a new list
+final_links = []
+for item in revised_list:
+    url = 'http://www.intgovforum.org'
+    concat = url + item
+    final_links.append(concat)
+
+# access the links in final_links list
+filenumber = 0 
+for url in final_links:
+    filenumber = filenumber + 1
+    r = requests.get(url, headers=headers)
+    print (url)
+    html = r.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+
+# extract the JavaScript from the transcript and only leave the text
+    for script in soup(["script", "style"]):
+        script.extract()   
+
+    text = soup.body.get_text()
+
+# further clean the text by deleting blank spaces between lines and encode the 
+# the final text in utf-8 format, in case there is any unknown letters
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    text = str(text.encode("ascii", "ignore"))
+
+# write the final product to a text file: only works for the last link in the list
+    #fob = open('/Users/oliviadziwak/Documents/Lab SC/2017/file_' + str(filenumber) + '.txt', 'w')
+    fob = open(FILE_DIR + 'file_' + str(filenumber) + '.txt', 'w')
+    fob.write(text)
+    fob.close()
+    print('Extracted and wrote transcript ' + str(filenumber) + ' to text file')
+
+# rename the files appropriately by their names as appears on the link
+for i in range(0, 117):
+    old_file = FILE_DIR + 'file_' + str(i + 1) + '.txt'
+    new_file = FILE_DIR + str(text_list[i]) + '.txt'
+    try:
+        os.rename(old_file, new_file)
+    except:
+        pass
