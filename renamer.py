64200669eb412dab52b895e6d109779b5d509641
Nasiff <nasif_fcb_haque@yahoo.com>
2019-01-20 11:42:12 -0500

    Added documentation

--- renamer.py
+++ renamer.py
@@ -1,5 +1,6 @@
 import os
 
+# Rename all the chunks in a format that is meaningful (Eg: Chunk_0037, Chunk_0038, ... etc.)
 for i in range(2006, 2018, 1):
     path = '/Users/User/Desktop/Mine/Workspace/vs_workspace/IGF_Mining/Transcripts_Chunked_Test/Chunked_' + str(i)
     print ("Renaming transcripts from the year " + str(i))
@@ -8,13 +9,3 @@ for i in range(2006, 2018, 1):
         postfix = filename[11:-4]
         zero_filled = postfix.zfill(4)
         os.rename(path + "/" + filename, path + "/" + prefix + zero_filled + ".txt")
-
-# for i in range(2011, 2012, 1):
-#     path = '/Users/User/Desktop/Mine/Workspace/vs_workspace/IGF_Mining/Transcripts_Chunked_Test/Chunked_' + str(i)
-#     num = 1229
-#     for filename in os.listdir(path):
-#         if "alt" in filename:
-#             num = num + 1
-#             prefix = filename[0:11]
-#             postfix = num
-#             os.rename(path + "/" + filename, path + "/" + prefix + str(postfix) + ".txt")
